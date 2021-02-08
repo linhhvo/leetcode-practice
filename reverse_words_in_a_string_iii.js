1c879d83448decd8452f715f696d3066cf691e19
Linh <lhvo2112@gmail.com>
2021-02-08 09:43:20 -0500

    Added comments

--- reverse_words_in_a_string_iii.js
+++ reverse_words_in_a_string_iii.js
@@ -3,10 +3,12 @@
  * @return {string}
  */
 var reverseWords = function (s) {
+  // Split given string into an array of words
   const sArray = s.split(' ');
-
+  // Transform each word in the array then join all words back into a string
   newWord = sArray.map(word => {
     let temp = '';
+    // Iterate over all characters in the word from the end and concat to the temporary string
     for (let i = word.length - 1; i >= 0; i--) {
       temp += word[i];
     }
