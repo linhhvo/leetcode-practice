6d6c344c58f02850e771cb6c5461bea3d50706c8
Linh <lhvo2112@gmail.com>
2021-02-10 13:59:01 -0500

    Complete number_complement

--- number_complement.js
+++ number_complement.js
@@ -3,25 +3,16 @@
  * @return {number}
  */
 var findComplement = function (num) {
-  // // Convert given number to binary string
-  // const binaryNum = num.toString(2);
+  // Convert given number to binary string
+  const binaryNum = num.toString(2);
 
-  // let temp = '';
-
-  // // Iterate over the string and convert 0 to 1 and 1 to 0
-  // for (let i = 0; i < binaryNum.length; i++) {
-  //   temp += binaryNum[i] === '0' ? '1' : '0';
-  // }
-
-  // // Convert the binary string back to number
   let result = '';
-  for (let i = num; i > 0; i >>= 1) {
-    console.log('i is ', i);
-    result += i ^ 1;
-    console.log('i^1 is ', i ^ 1);
-    console.log('result is ', result);
+
+  // Iterate over the string and convert 0 to 1 and 1 to 0
+  for (let i = 0; i < binaryNum.length; i++) {
+    result += binaryNum[i] === '0' ? '1' : '0';
   }
+
+  // Convert the binary string back to number
   return parseInt(result, 2);
 };
-
-findComplement(5);
